b55881c42f78d7faff3671fb27823f53f96c1934
Sarthak <saryatnam22@gmail.com>
2024-05-21 23:06:06 +0530

    initialized file and folder structure, shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
-
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+    <div className = "h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className = "text-2xl text-white">
+        LiveCraft
+      </h1>
+    </div>
   );
 }
