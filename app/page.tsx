8810c2ab46df803f95c4ff19da5805439fed06f1
Sarthak <saryatnam22@gmail.com>
2024-05-23 00:34:05 +0530

    added canvas element and initialized all useRef hooks

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,43 @@ import LeftSideBar from "@/components/LeftSideBar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSideBar from "@/components/RightSideBar";
+import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { CustomFabricObject } from "@/types/type";
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>('rectangle');
+
+  useEffect(() => {
+    const canvas = initializeFabric({canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas, 
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+
+    window.addEventListener("resize", () => {
+      handleResize({ canvas: fabricRef.current, })
+    })
+  }, [])
+
   return (
     <main className = "h-screen overflow-hidden"  >
         <Navbar />
 
         <section className = "flex h-full flex-row" >
           <LeftSideBar />
-            <Live />
+            <Live canvasRef = { canvasRef } />
           <RightSideBar />
         </section>
     </main>
--- components/Live.tsx
+++ components/Live.tsx
@@ -7,7 +7,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+  canvasRef : React.MutableRefObject<HTMLCanvasElement | null>;
+}
+
+const Live = ({ canvasRef } : Props) => {
   const others = useOthers();
   const [ { cursor }, updateMyPresence] = useMyPresence() as any;
   const [cursorState, setCursorState] = useState<CursorState>({
@@ -135,13 +139,14 @@ const Live = () => {
 
   return (
     <div
+      id = "canvas"
       onPointerMove = { handlePointerMove }
       onPointerLeave = { handlePointerLeave }
       onPointerDown = { handlePointerDown }
       onPointerUp = { handlePointerUp }
       className = "h-[100vh] w-full flex justify-center items-center text-center"
     >
-      <h1 className = "text-2xl text-white" >LiveCraft</h1>
+      <canvas ref = { canvasRef } />
 
       { reactions.map((reaction) => (
         <FlyingReaction 
