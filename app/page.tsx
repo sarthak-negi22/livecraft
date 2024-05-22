463a10d7be4345fe634c0dc84f3b68bb9b90581a
Sarthak <saryatnam22@gmail.com>
2024-05-22 20:16:56 +0530

    added chat cursor functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
   return (
-    <div className = "h-[100vh] w-full flex justify-center items-center text-center">
-      <h1 className = "text-2xl text-white">
-        LiveCraft
-      </h1>
+    <div>
+        <Live />
     </div>
   );
 }
