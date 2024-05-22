99eaf706d74915fc38f214eca7ca39566924a4dc
Sarthak <saryatnam22@gmail.com>
2024-05-23 00:01:43 +0530

    added active users liveblocks functionality and initialized navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Image from "next/image";
+import { memo } from "react";
+
+import { NavbarProps } from "@/types/type";
+import ActiveUsers from "./users/ActiveUsers";
+
+const Navbar = ({ activeElement } : NavbarProps) => {
+  return (
+    <nav className = "flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
+        <Image 
+            src = "/assets/favicon.ico"
+            alt = "logo"
+            width = { 58 }
+            height = { 20 }
+        />
+        <ActiveUsers />
+    </nav>
+  )
+}
+
+export default memo(Navbar, (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement);
