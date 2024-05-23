fbf7c5f27d41627f96a57668f1e5e6218e131fce
Sarthak <saryatnam22@gmail.com>
2024-05-23 18:27:02 +0530

    added real-time clear, delete functionality

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSideBar from "@/components/RightSideBar";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from "@/lib/canvas";
+import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
 import { ActiveElement, CustomFabricObject } from "@/types/type";
 import { useEffect, useRef, useState } from "react";
 import { fabric } from "fabric";
+import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete } from "@/lib/key-events";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -15,16 +18,63 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>('rectangle');
+  const activeObjectRef = useRef<fabric.Object | null>(null);
+
+  const canvasObjects = useStorage((root) => root.canvasObjects);
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if(!object) return;
+
+    const { objectId } = object;
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.set(objectId, shapeData);
+  }, [])
 
   const [activeElement, setActiveElement] = useState<ActiveElement>({
     name : '',
     value : '',
-    icon : ''
+    icon : '',
   });
+
+  const deleteAllShapes = useMutation(({storage}) => {
+    const canvasObjects = storage.get('canvasObjects');
+
+    if(!canvasObjects || canvasObjects.size === 0)  return true;
+
+    for(const [key,value] of canvasObjects.entries()) {
+      canvasObjects.delete(key)
+    }
+
+    return canvasObjects.size === 0;
+  }, [])
+
+  const deleteShapeFromStorage = useMutation(({storage}, objectId) => {
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.delete(objectId);
+  }, [])
 
   const handleActiveElement = (elem : ActiveElement) => {
     setActiveElement(elem);
 
+    switch(elem?.value) {
+      case 'reset':
+        deleteAllShapes();
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+
+      case 'delete':
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage);
+        setActiveElement(defaultNavElement);
+        break;
+
+      default:
+        break;
+    }
     selectedShapeRef.current = elem?.value as string; 
   }
 
@@ -41,16 +91,60 @@ export default function Page() {
       });
     });
 
+    canvas.on("mouse:move", (options) => {
+      handleCanvaseMouseMove({
+        options,
+        canvas, 
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+      });
+    });
+
+    canvas.on("mouse:up", (options) => {
+      handleCanvasMouseUp({
+        canvas, 
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+        syncShapeInStorage,
+        setActiveElement,
+        activeObjectRef,
+      });
+    });
+
+    canvas.on("object:modified", (options) => {
+      handleCanvasObjectModified({
+        options,
+        syncShapeInStorage,
+      })
+    })
+    
     window.addEventListener("resize", () => {
       handleResize({ canvas: fabricRef.current, })
     })
+
+    return () => {
+      canvas.dispose();
+    }
   }, [])
+
+
+  useEffect(() => {
+    renderCanvas({
+      fabricRef,
+      canvasObjects,
+      activeObjectRef,
+    })
+  }, [canvasObjects])
 
   return (
     <main className = "h-screen overflow-hidden"  >
         <Navbar 
           activeElement = { activeElement }
           handleActiveElement = { handleActiveElement }
+
         />
 
         <section className = "flex h-full flex-row" >
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import { Button } from "./ui/button";
 import ShapesMenu from "./ShapesMenu";
 import ActiveUsers from "./users/ActiveUsers";
 import { NewThread } from "./comments/NewThread";
+import Link from "next/link";
 
 const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps) => {
   const isActive = (value: string | Array<ActiveElement>) =>
@@ -18,10 +19,15 @@ const Navbar = ({ activeElement, imageInputRef, handleImageUpload, handleActiveE
 
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
-      <Image src="/assets/favicon.ico" alt="FigPro Logo" width={58} height={20} />
+      <Link href = "/" >
+        <div className = "flex items-center justify-center" >
+          <Image src="/assets/favicon.ico" alt="FigPro Logo" width={58} height={20} />
+          <h3 className="px-2 py-4 text-xl">LiveCraft</h3>
+        </div>
+      </Link>
 
       <ul className="flex flex-row">
-        {navElements.map((item: ActiveElement | any) => (
+        { navElements.map((item: ActiveElement | any) => (
           <li
             key={item.name}
             onClick={() => {
