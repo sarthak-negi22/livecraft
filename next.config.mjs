1b4d4ff055b78fb4896d33168980ea8329439df3
Sarthak <saryatnam22@gmail.com>
2024-05-25 02:16:35 +0530

    disabled typescript ignoreBuildErrors in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,9 @@ const nextConfig = {
         },
       ],
     },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
+    // typescript: {
+    //   ignoreBuildErrors: true,
+    // },
   };
   
   export default nextConfig;
