5366e4e69e6ba4df5f84e1ad11c04ef4579ec700
Sarthak <saryatnam22@gmail.com>
2024-05-25 02:25:13 +0530

    fixed active users and avatar components

--- components/users/Avatar.tsx
+++ components/users/Avatar.tsx
@@ -1,18 +1,32 @@
-import React from "react";
-import styles from "./Avatar.module.css";
 import Image from "next/image";
 
-const IMAGE_SIZE = 48;
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "../ui/tooltip";
 
-export function Avatar({ otherStyles, name }: { otherStyles: string; name: string }) {
-  return (
-    <div className={`${styles.avatar} ${otherStyles} h-9 w-9 `} data-tooltip={name}>
-      <Image
-        src = {`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
-        fill
-        className = { styles.avatar_picture }
-        alt = { name }
-      />
-    </div>
-  );
-}
+type Props = {
+  name: string;
+  otherStyles?: string;
+};
+
+const Avatar = ({ name, otherStyles }: Props) => (
+  <>
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className={`relative h-9 w-9 rounded-full ${otherStyles}`} data-tooltip={name}>
+          <Image
+            src={`https://liveblocks.io/avatars/avatar-${Math.floor(Math.random() * 30)}.png`}
+            fill
+            className="rounded-full"
+            alt={name}
+          />
+        </div>
+      </TooltipTrigger>
+      <TooltipContent className="border-none bg-primary-grey-200 px-2.5 py-1.5 text-xs">
+        {name}
+      </TooltipContent>
+    </Tooltip>
+    </TooltipProvider>
+  </>
+);
+
+export default Avatar;
